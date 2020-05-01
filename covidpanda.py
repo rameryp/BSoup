50d03ae857d2be43240c9b5457a88275a9f690af
Pierre Macbook <ramerpie@gmail.com>
2020-05-01 12:49:09 +0200

    save

--- covidpanda.py
+++ covidpanda.py
@@ -4,7 +4,7 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-import random
+import pandas as pd
 
 def clearScreen():
   print(chr(27)+'[2j')
@@ -24,57 +24,70 @@ table_rows = full_content.find_all('tr')
 
 country_data =[]
 
-def myInt(a):
-    if a:
-          return a
-    else:
-          return 0
+def mk_int(s):
+    s = s.strip()
+    if s =='N/A':
+          s=0 
+    return float(s) if s else 0
 
+def cleanArray(dirtyArray):
+      cleanedArray = []
+      cleanedArray = [x.replace(',', '') for x in dirtyArray]
+      cleanedArray = [x.replace('+', '') for x in cleanedArray]
+      return cleanedArray
+    
+    
 for tr in table_rows[9:-9]:  #skip regions row at the beginning and total rows at the end
   # get all rows
   td = tr.find_all('td')
   # remove trailing spaces
   row = [i.text.strip() for i in td]
-  #clean up rows
-  row[1:11] = [x.replace(',', '') for x in row[1:11]]
-  row[1:11] = [x.replace('+', '') for x in row[1:11]]
-  # convert string to int while managing empty value 
-  row[1:11] = map(myInt,row[1:11])
+  row[1:11] = cleanArray(row[1:11])
+  row[1:11] = map(mk_int,row[1:11])
   # add cleaned row
   country_data.append(row)
-  
-#print(country_data)
 
 
 ####################################################################
 
 def main():  
-  clearScreen()
+  #clearScreen()
   Countries  = []
   TotalCases = []	
   NewCases	 = []	
   TotalDeaths= []	
   NewDeaths	 = []	
-
+  TotalRecovered  = []	
+  ActiveCases	 = []	
+  
   for row in country_data:
-    #if row: #not empty
-    #print (row[0]+' - '+str(row[1])+' - '+str(row[2])+' - '+str(row[3])+' - '+str(row[4])+' - '+str(row[5])+' - '+str(row[6]))
     Countries.append(row[0])
     TotalCases.append(row[1])
     NewCases.append(row[2])
     TotalDeaths.append(row[3])
     NewDeaths.append(row[4])
+    TotalRecovered.append(row[5])
+    ActiveCases.append(row[6])
 
-  print(TotalCases)
-  print(Countries)
+  df = pd.DataFrame({
+    'Countries':Countries,
+    'TotalCases':TotalCases,
+    'NewCases':NewCases,
+    'TotalDeaths':TotalDeaths,
+    'NewDeaths':NewDeaths,
+    'TotalRecovered':TotalRecovered,
+    'ActiveCases':ActiveCases,
+    })
+
+  
+  print(df)
 
 if __name__ == "__main__":
       main()
 
 
 
-#TotalRecovered  = []	
-#ActiveCases	 = []	
+
 #SeriousCritical	 = []	
 #TotCases1M  = []	
 #Deaths1Mpop = []	
